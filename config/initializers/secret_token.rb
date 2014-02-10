3af429dc015041db0ad7a83002ca36796de556d1
Eliza Brock <eliza@elizabrocksoftware.com>
2014-02-10 10:23:08 -0600

    Initialize rails: `rails new . -T --database=postgresql`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squawker::Application.config.secret_key_base = '872bf83fcee761ce84bf8bddc1a38f8ce73a2ce53da82ef60a380453d63a27df9a42d2e3ec3fadbbb99e551b7424b1fa95b198b8526d63f3d22987a339b5efbc'
