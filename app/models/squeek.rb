d4eb3d487a53378e93665f72fe6ad7254d16db5e
Brian Arenz <brianarenz@gmail.com>
2014-02-13 15:32:04 -0600

    Add mention in squeek creates link to mentioned user profile

--- app/models/squeek.rb
+++ app/models/squeek.rb
@@ -3,4 +3,31 @@ class Squeek < ActiveRecord::Base
   validates_presence_of :user
 
   belongs_to :user
+
+  after_validation :save_mentions, on: :create
+
+  protected
+    def save_mentions
+      if has_mentions
+        @mentions.each do |mention|
+          mention[0] = ''
+          mentioned_user = User.where(username: mention.downcase).first
+          if mentioned_user
+            new_string = "<a href='/users/#{mentioned_user.id}'>@#{mention}</a>"
+            self.body = self.body.gsub("@#{mention}",new_string)
+          end
+        end
+      end
+    end
+
+    def has_mentions
+      reg = /(?:@\w+)/
+      mentions = self.body.scan(reg).uniq
+      if mentions.length > 0
+        @mentions = mentions
+        true
+      else
+        false
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,14 @@ class User < ActiveRecord::Base
 
   has_many :squeeks
 
+  after_validation :normalize_username, on: :create
+
   def self.find_for_database_authentication(conditions)
     self.where(:username => conditions[:email]).first || self.where(:email => conditions[:email]).first
   end
+
+  protected
+    def normalize_username
+      self.username = self.username.downcase
+    end
 end
