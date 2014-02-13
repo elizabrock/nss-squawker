81fe35c24cb55c4630e91bbc0f76668c77cb2717
Drew Blumberg <alblumberg21@gmail.com>
2014-02-13 17:03:24 -0600

    Initial relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :squeeks
+  has_many :relationships, foreign_key: "consumer_id"
+  has_many :reverse_relationships, foreign_key: "broadcaster_id", class_name: "Relationship"
+  has_many :consumers, through: :relationships, source: :consumer
+  has_many :broadcasters, through :reverse_relationships, source: :broadcaster
 
   def self.find_for_database_authentication(conditions)
     self.where(:username => conditions[:email]).first || self.where(:email => conditions[:email]).first
+  end
+
+  def consuming?(broadcaster)
+    relationships.find_by(broadcaster_id: broadcaster.id)
+  end
+
+  def consume!(broadcaster)
+    relationships.create!(broadcaster_id: broadcaster.id])
+  end
+
+  def unconsume(broadcaster)
+    relationships.find_by(broadcaster_id: broadcaster.id)
   end
 end
