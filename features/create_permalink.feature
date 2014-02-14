04fe4b53666f6ab6da9857a69339b47a2c1f0638
millerkatiej <millerkatiej@gmail.com>
2014-02-14 10:28:10 -0600

    trying to fetch upstream

--- features/create_permalink.feature
+++ features/create_permalink.feature
@@ -3,7 +3,8 @@ Feature: Permalink
 	As a user
 	I want see the correct URL for the page and post that I am viewing
 
-Scenario: User "millerkatiej" logins in
-	Given I press "My Profile" link
-	Then I am redirected to my profile page
+Scenario: User "millerkatiej" visits their profile page
+	Given I am logged in as "millerkatiej"
+	When I press "My Profile" link
+	And I am redirected to my profile page
 	Then the URL will display as "localhost:3000/users/millerkatiej" 
--- features/step_definitions/permalink_steps.rb
+++ features/step_definitions/permalink_steps.rb
@@ -1,8 +1,15 @@
-Given(/^I'm signed in as "?(.*?)"?$/) do |username|
-  @user = Fabricate(:user, username: username)
-  visit new_user_session_path
-  fill_in "Email / Username", with: username
-  fill_in "Password", with: "password"
-  click_button "Sign in"
-  page.should have_content("Signed in successfully")
+Given(/^I am logged in as "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)" link$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am redirected to my profile page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the URL will display as "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
 end
