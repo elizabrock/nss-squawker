4ffdbdc8177280e5fcb783a43d1814a0e9be36f8
LeahStevens <leiastevens@hotmail.com>
2014-02-14 10:18:03 -0600

    Created another test for autocomplete

--- features/step_definitions/autosuggest_steps.rb
+++ features/step_definitions/autosuggest_steps.rb
@@ -6,3 +6,14 @@ Then /^I should not see "(.*?)" within the username dropdown field$/ do |arg1|
   pending
 end
 
+When(/^I type the squeek "(.*?)"$/) do |arg1|
+  pending
+end
+
+When(/^I choose "(.*?)" in the autocomplete list$/) do |arg1|
+  pending
+end
+
+Then(/^the "(.*?)" field should contain "(.*?)"$/) do |arg1, arg2|
+  pending
+end
