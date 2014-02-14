3524b01784d2ec1374f5588466e9b37c68c5c654
millerkatiej <millerkatiej@gmail.com>
2014-02-14 11:31:11 -0600

    cucumber test passinggit add .! :star:

--- features/step_definitions/permalink_steps.rb
+++ features/step_definitions/permalink_steps.rb
@@ -2,3 +2,15 @@ Given(/^I am on the homepage$/) do
   visit root_path
 end
 
+When(/^I press "(.*?)" link$/) do |arg1|
+  click_link('My Profile')
+end
+
+When(/^I am redirected to my profile page$/) do
+	visit('/users/millerkatiej')
+end
+
+Then(/^the URL will display as "(.*?)"$/) do |arg1|
+page.has_xpath?('/users/millerkatiej')
+end
+
