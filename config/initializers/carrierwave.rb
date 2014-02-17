6b517d0ca9c3227433f4959d4ae558bfa265bbdc
amanda-thrive <amanda@thrivemg.com>
2014-02-17 11:50:20 -0600

    setup storage for production to be fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,4 +3,15 @@ if Rails.env.test? or Rails.env.cucumber?
     config.storage = :file
     config.enable_processing = false
   end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider              => 'AWS',
+      :aws_access_key_id     => 'xxx',
+      :aws_secret_access_key => 'yyy',
+    }
+    config.fog_directory = 'uploads'
+    config.fog_public    = false
+  end
 end
--- config/initializers/fog.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'secret',
-    :aws_secret_access_key  => 'secret',
-  }
-  config.fog_directory  = 'uploads'
-  config.fog_public     = false
-end
