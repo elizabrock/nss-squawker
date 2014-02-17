208ae5a290cd10a3d5222587cd6b2c409f9e5360
Brian Arenz <brianarenz@gmail.com>
2014-02-16 23:05:58 -0600

    Mention now case-insensitive, email only occurs AFTER successful db create

--- app/models/squeek.rb
+++ app/models/squeek.rb
@@ -6,27 +6,35 @@ class Squeek < ActiveRecord::Base
 
   after_validation :create_mentions, on: :create
 
+  after_create :mention_notify
+
   protected
     def create_mentions
-      if find_mentions
-        @mentions.each do |mention|
-          mention[0] = ''
-          mentioned_user = User.where(username: mention.downcase).first
-          if mentioned_user
-            new_string = "<a href='http://localhost:3000/users/#{mentioned_user.id}'>@#{mention}</a>"
-            self.body = self.body.gsub("@#{mention}",new_string)
-            mention_notify(mentioned_user)
-          end
+      find_mentions
+      unless @mentions.empty?
+        @mentions.each do |mentioned_user|
+          new_string = "<a href='http://localhost:3000/users/#{mentioned_user.id}'>@#{mentioned_user.username}</a>"
+          self.body = self.body.gsub(/(@#{mentioned_user.username})/i,new_string)
         end
       end
     end
 
     def find_mentions
+      @mentions = []
       reg = /(?:@\w+)/
-      @mentions = self.body.scan(reg).uniq
+      mentions = self.body.scan(reg).uniq
+      mentions.each do |mention|
+        mention[0] = ''
+        mentioned_user = User.find_by_username(mention.downcase)
+        @mentions << mentioned_user unless mentioned_user.nil?
+      end
     end
 
-    def mention_notify mentioned_user
-      NotificationMailer.mention_email(mentioned_user,self.user,self.body).deliver
+    def mention_notify
+      unless @mentions.empty?
+        @mentions.each do |mentioned_user|
+          NotificationMailer.mention_email(mentioned_user,self.user,self.body).deliver
+        end
+      end
     end
 end
