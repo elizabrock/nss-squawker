7dd7fc43219ad6aa3ca37b5872ff032b5bb2e94b
Pete Dunlap <petedunlap@gmail.com>
2014-02-17 11:51:40 -0600

    deleted excess comments

--- features/support/omniauth.rb
+++ features/support/omniauth.rb
@@ -1,12 +1,6 @@
 OmniAuth.config.test_mode = true
-# OmniAuth.config.add_mock(:twitter, {
-#  :uid => "123545",
-#  :username => "@pete"
-# })
-
 OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
   :provider => 'twitter',
   :uid => '123545',
   :nickname => 'bopperz'
-  # etc.
 })
