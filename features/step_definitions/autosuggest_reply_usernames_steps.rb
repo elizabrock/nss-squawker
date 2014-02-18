9bc64e26d9de3344335bad7aefe3cd15cd87a8c4
Eliza Brock <eliza@elizabrocksoftware.com>
2014-02-18 17:46:35 -0600

    Another attempt at debugging the CI issues.

--- features/step_definitions/autosuggest_reply_usernames_steps.rb
+++ features/step_definitions/autosuggest_reply_usernames_steps.rb
@@ -4,6 +4,15 @@ When /^I key in "(.*?)" with "(.*?)"$/ do |field, content|
   end
 end
 
+When(/^I wait for the request to complete$/) do
+  Timeout.timeout(Capybara.default_wait_time) do
+    loop do
+      active = page.evaluate_script('jQuery.active')
+      break if active == 0
+    end
+  end
+end
+
 Then(/^"(.*?)" should be filled in with "(.*?)"$/) do |field_name, text|
   page.should have_field(field_name, with: text)
 end
