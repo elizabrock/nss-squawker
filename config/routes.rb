63f4f11489ade2c68db169327e05fe2b2a3a6e27
Eliza Brock <eliza@elizabrocksoftware.com>
2014-02-20 09:35:58 -0600

    Duplicated controller?! gone!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Squawker::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => :registrations }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :registrations => :registrations }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
